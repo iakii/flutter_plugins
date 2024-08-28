1baef44667158a2ab18799b1e5869a16d90729cd
iAkii <xiongkai@petalmail.com>
2024-08-28 14:56:46 +0800

    feat(core): 添加方法.

--- rust_ffi_lib/lib/rust_ffi_lib.dart
+++ rust_ffi_lib/lib/rust_ffi_lib.dart
@@ -1,4 +1,17 @@
 library rust_ffi_lib;
 
-export 'src/rust/api/simple.dart';
-export 'src/rust/frb_generated.dart' show RustLib;
+import 'package:rust_ffi_lib/src/rust/api/paste.dart' as hotkey;
+import 'package:rust_ffi_lib/src/rust/api/sys.dart' as sys;
+import 'package:rust_ffi_lib/src/rust/frb_generated.dart';
+
+class _RustFFILib {
+  _RustFFILib() {
+    RustLib.init();
+  }
+
+  Future<void> paste() => hotkey.paste();
+  Future<void> insert() => hotkey.insert();
+  Future<String?> findPid(String processName) => sys.findPid(name: processName);
+}
+
+final rustFFILib = _RustFFILib();
